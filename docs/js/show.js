4a4a2d37c1a15fb0c01f07d46c1ff9663eea2e85
Donald St. Martin <dstmartin@tamu.edu>
2021-11-07 23:32:24 -0600

    Add show single contact page

---
+++ docs/js/show.js
@@ -0,0 +1,48 @@
+var db = new Dexie("ContactsDB");
+db.version(4).stores({
+    contacts: '++ID, first_name, last_name, timestamp'
+});
+
+db.open().catch(function (e) {
+    console.error("Open failed: " + e.stack);
+})
+
+const urlParams = new URLSearchParams(window.location.search);
+const getID = urlParams.get('id');
+
+function getContactByID(id) {
+    
+    return db.transaction('r', [db.contacts], async () => {
+        id = parseInt(id);
+        const contact = await db.contacts.get(id);
+
+        const fullName = contact.first_name + ' ' + contact.last_name;
+        var date = new Date(contact.timestamp);
+        const month = date.toLocaleString('default', { month: 'long' });
+        var formattedDate = month + ' ' + date.getDate() + ', ' + date.getFullYear();
+
+        document.getElementById('contact-name').innerHTML = fullName;
+        
+        if (contact.home_phone)
+            document.getElementById('home-phone').innerHTML = contact.home_phone;
+
+        if (contact.cell_phone)
+            document.getElementById('cell-phone').innerHTML = contact.cell_phone;
+
+        if (contact.work_phone)
+            document.getElementById('work-phone').innerHTML = contact.work_phone;
+
+        if (contact.email)
+            document.getElementById('email').innerHTML = contact.email;
+        
+        document.getElementById('date-added').innerHTML = formattedDate;
+    });
+}
+
+if (getID)
+    getContactByID(getID);
+else
+    // Need an ID before we show any user details
+    // TODO: Check if ID exists and also redirect if not
+    window.location.replace("index.html");
+
